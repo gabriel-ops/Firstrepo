7403e84cdf083c2898eb1d1827f2869840b55fb8
Gabriel <gabrielpinzariu@yahoo.com>
2019-11-16 12:18:52 +0200

    initialcommit

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //int sum = sum();
+            //Console.WriteLine(Sum());
+
+            Sum();
+            int first = 7;
+            int second = 10;
+            Console.WriteLine(Multiply(first, second));
+        }
+        static void Sum()
+        {
+            int a = 2;
+            int b = 3;
+
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        
+        static void DisplayNumbers(in n)
+        { for (int =1 ; i <=n; i++)
+                Console.WriteLine();
+
+           
+          
+
+         
+            
+       
+            
+      
+
+        
+       
+    
+    
+    
+    
+    }
+        
+        
+       
+
+       
+
+        
+
+}
---
+++ Methods/Methods2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a number :");
+            string firstNumber  = Console.ReadLine();
+            Console.WriteLine( PLease write another number:");
+                string lastNumber
+            
+                int number1 = int.Parse(fisrtNumber);
+            int number 2 = int.Parse(lastNumber);
+            
+          
+            Console.WriteLine("The sum is:"+ number1 +number2+ "?!");
+        }
+    }
+}
